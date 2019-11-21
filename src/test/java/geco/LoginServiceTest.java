7ee2c7cc43a1e879f40f4bd9e7e181787dc80110
dlm2213a <emeric.delacroix@univ-tlse3.fr>
2019-11-21 11:13:17 +0100

    commit LoginServiceTest

--- src/test/java/geco/LoginServiceTest.java
+++ src/test/java/geco/LoginServiceTest.java
@@ -3,13 +3,21 @@ package geco;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import sun.rmi.runtime.Log;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
 public class LoginServiceTest {
 
+    private LoginService logins;
+
     @Before
     public void setUp() throws Exception {
+        String[] loginsExistants = new String[]{"login1", "login2", "identifiant"};
+        logins = new LoginService(loginsExistants);
     }
 
     @After
@@ -17,18 +25,36 @@ public class LoginServiceTest {
     }
 
     @Test
-    public void loginExists() {
+    public void testLoginExists() {
+        assertTrue(logins.loginExists("login1"));
     }
 
     @Test
-    public void addLogin() {
+    public void testLoginDoesNotExists() {
+        assertFalse(logins.loginExists("login10"));
+    }
+
+    @Test
+    public void testAddLogin() {
+        logins.addLogin("login3");
+        assertTrue("", logins.loginExists("login3"));
     }
 
     @Test
     public void findAllLoginsStartingWith() {
+        List loginsStart = new ArrayList(){};
+        loginsStart = logins.findAllLoginsStartingWith("log");
+        assertTrue(loginsStart.contains("login1"));
+        assertTrue(loginsStart.contains("login2"));
+        assertFalse(loginsStart.contains("identifiant"));
     }
 
     @Test
-    public void findAllLogins() {
+    public void testFindAllLogins() {
+        List loginsAll = new ArrayList(){};
+        loginsAll = logins.findAllLogins();
+        assertTrue(loginsAll.contains("login1"));
+        assertTrue(loginsAll.contains("login2"));
+        assertTrue(loginsAll.contains("identifiant"));
     }
 }
